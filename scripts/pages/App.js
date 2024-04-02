d8dd6003a73de62e160073013e00d8b026edfe09
aymmc <aymericlevaillant@gmail.com>
2024-04-02 15:23:04 +0200

    css correct

--- scripts/pages/App.js
+++ scripts/pages/App.js
@@ -11,41 +11,41 @@ class App {
         const nombreDeRecettes = this.RecipesData.length;
         console.log('Nombre de recettes:', nombreDeRecettes);
         console.log('Données des recettes:', this.RecipesData); // Afficher les données récupérées dans la console
-     
+
         return this.RecipesData;
-        
+
     }
     flechechangement() {
         document.addEventListener('DOMContentLoaded', function () {
             var filterbars = document.querySelectorAll(".dropdown-toggle");
-        
+
             filterbars.forEach((filterbar) => {
-                filterbar.addEventListener('shown.bs.dropdown', (event)=> {
+                filterbar.addEventListener('shown.bs.dropdown', (event) => {
                     var filterImg = event.currentTarget.querySelector("img.fleche");
                     filterImg.style.transform = "rotate(180deg)";
                 });
-        
-                filterbar.addEventListener('hidden.bs.dropdown',  (event)=>  {
+
+                filterbar.addEventListener('hidden.bs.dropdown', (event) => {
                     var filterImg = event.currentTarget.querySelector("img.fleche");
                     filterImg.style.transform = "rotate(0deg)";
                 });
             });
         });
     }
-    
-    
-    afficherRecette() { 
-   
-        this.RecipesData.forEach((RecipesData) => {
-           
+
+
+    afficherRecette() {
+
+         this.RecipesData.forEach((RecipesData) => {
+
             this.Fabrik = new Fabrik()
-          
-            
-            this.Fabrik.createCarte(RecipesData.name, RecipesData.description, RecipesData.ingredients, RecipesData.image, RecipesData.time )
+
+
+            this.Fabrik.createCarte(RecipesData.name, RecipesData.description, RecipesData.ingredients, RecipesData.image, RecipesData.time)
         });
     }
-    afficherresultat(){
-        this.Fabrik = new Fabrik() 
+    afficherresultat() {
+        this.Fabrik = new Fabrik()
         this.Fabrik.createTotal(this.RecipesData.length)
     }
     async main() {
